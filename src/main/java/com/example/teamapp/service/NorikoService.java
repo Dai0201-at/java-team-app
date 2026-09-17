49c89b30b1f179f0b628dfa311e931a5910c52e6
3030750 <3030750@30307-50>
2026-09-17 14:25:18 +0900

    自己紹介を追加

---
+++ src/main/java/com/example/teamapp/service/NorikoService.java
@@ -0,0 +1,11 @@
+package com.example.teamapp.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class NorikoService {
+	public String getProfile() {
+        return "メンバーの田中です！趣味は趣味はドライブです。";
+    }
+
+}
